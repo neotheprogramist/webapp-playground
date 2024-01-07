f46ef384d5a4a71ac3b8e566139c4c2b2f5f2829
kacpergumieniuk <kacpergumieniuk@gmail.com>
2024-01-07 12:53:17 +0100

    Add tailwind, create task route, remove demo app

---
+++ src/routes/task/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div class="h-screen w-screen bg-black text-white text-xl flex justify-center items-center">
+      Hello World!
+    </div>
+  );
+});
